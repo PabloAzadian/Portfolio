a38505ec72b59035e8b3a9a31d9c022dcb870cd8
PabloAzadian <pabloazadian09@gmail.com>
2024-07-19 23:56:43 +0200

    syntax changes

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -29,7 +29,7 @@ function About() {
 
                     <h4> > I've always been a computer geek ever since I was seven years old. My journey into the world of programming
                     started when I was fifteen, and I discovered the magic of bringing ideas to life with a few lines of code. From my
-                    first <span>"Hello World"</span> application, I knew I had found my passion. Software development is not just a job for me; it's an
+                    first "Hello World" application, I knew I had found my passion. Software development is not just a job for me; it's an
                     exciting challenge that keeps me learning and pushing my limits every day. Below, you'll find projects I've completed
                     that showcase my skills and dedication to creating high-quality software.
                     </h4>
